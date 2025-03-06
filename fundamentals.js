7df2724c7cb18b53835ef7eda64e17e20220ef43
CorruptedMonk7 <rayhan.uddin.aiub@gmail.com>
2025-03-06 17:17:51 +0600

    first commit

---
+++ fundamentals.js
@@ -0,0 +1,16 @@
+// How to declare variables using let, const
+
+const name = "Rayhan";
+let age = 26;
+age = 27;
+
+console.log(age)
+
+//How to use conditions
+
+if (name === "Rayhan" && age === 27) {
+    console.log("Right Person")
+}
+else (
+    console.log("Wrong Person")
+)
