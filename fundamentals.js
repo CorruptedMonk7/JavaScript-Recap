6be5ffeb312b777a0a4275eedaad09570ce2629a
CorruptedMonk7 <rayhan.uddin.aiub@gmail.com>
2025-03-06 17:33:21 +0600

    last commit

--- fundamentals.js
+++ fundamentals.js
@@ -14,3 +14,37 @@ if (name === "Rayhan" && age === 27) {
 else (
     console.log("Wrong Person")
 )
+
+// Array Operations 
+
+const numbers = [7, 5, 9, 4, 3, 4, 8, 0]
+let length = numbers.length;
+console.log(length)
+console.log(numbers.indexOf(8))
+numbers[6] = 1;
+console.log(numbers)
+
+// loop
+
+for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i])
+}
+
+// function 
+
+function multiply(num1, num2) {
+    let result = num1 * num2;
+    return result;
+}
+const callingFunction = multiply(7, 5)
+console.log("Result is:", callingFunction)
+
+// Object 
+
+const student = {
+    name: "Rayhan",
+    lastName: "Uddin",
+    age: 27,
+    profession: "student"
+}
+console.log(student.name)
